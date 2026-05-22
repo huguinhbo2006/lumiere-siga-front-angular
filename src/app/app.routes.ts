678a75050dff48fa010aae4c1097629ea0b3c12f
huguinhbo2006 <hugo.percar53@gmail.com>
2026-05-22 12:17:15 -0600

    Angular confiurado para ui

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,19 @@
 import { Routes } from '@angular/router';
+import { AdminLayoutComponent } from './layout/admin-layout/admin-layout.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+
+  {
+    path: '',
+    component: AdminLayoutComponent,
+    children: [
+      {
+        path: '',
+        loadComponent: () =>
+          import('./features/dashboard/pages/home/home.component')
+            .then(m => m.HomeComponent)
+      }
+    ]
+  }
+
+];
