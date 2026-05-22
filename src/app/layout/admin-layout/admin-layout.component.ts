2ed9a1e2817e3c741dcd4c27de2ddcf7aa62cb0e
huguinhbo2006 <hugo.percar53@gmail.com>
2026-05-22 12:30:44 -0600

    creacionde layout y ui card

--- src/app/layout/admin-layout/admin-layout.component.ts
+++ src/app/layout/admin-layout/admin-layout.component.ts
@@ -1,8 +1,15 @@
 import { Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+import { SidebarComponent } from '../sidebar/sidebar.component';
+import { NavbarComponent } from '../navbar/navbar.component';
 
 @Component({
   selector: 'app-admin-layout',
-  imports: [],
+  imports: [
+    RouterOutlet,
+    SidebarComponent,
+    NavbarComponent
+  ],
   templateUrl: './admin-layout.component.html',
   styleUrl: './admin-layout.component.scss',
 })
