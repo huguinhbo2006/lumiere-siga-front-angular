d5fbc67cc78977412f881ccb3cae1c19847ea593
huguinhbo2006 <hugo.percar53@gmail.com>
2026-05-22 19:53:09 -0600

    feat: agregar search y sorting enterprise a ui-table

---
+++ src/app/models/table-column.model.ts
@@ -0,0 +1,27 @@
+export interface TableColumnModel {
+
+  key: string;
+
+  label: string;
+
+  width?: string;
+
+  align?: 'left' | 'center' | 'right';
+
+  sortable?: boolean;
+
+  type?:
+    | 'text'
+    | 'badge'
+    | 'date'
+    | 'currency'
+    | 'actions';
+
+  badgeVariant?:
+    | 'success'
+    | 'danger'
+    | 'warning'
+    | 'primary'
+    | 'secondary';
+
+}
