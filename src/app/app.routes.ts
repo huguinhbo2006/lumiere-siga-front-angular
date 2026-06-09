b982095469ac2e1a13ad85c901757254cb941622
huguinhbo2006 <hugo.percar53@gmail.com>
2026-06-09 13:26:12 -0600

    feat: add auth layout and login page

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,8 +1,22 @@
 import { Routes } from '@angular/router';
 
 import { AdminLayoutComponent } from './layout/admin-layout/admin-layout.component';
+import { AuthLayoutComponent } from './layout/auth-layout/auth-layout.component';
 
 export const routes: Routes = [
+
+  {
+    path: 'login',
+    component: AuthLayoutComponent,
+    children: [
+      {
+        path: '',
+        loadComponent: () =>
+          import('./features/auth/pages/login/login.component')
+            .then(m => m.LoginComponent)
+      }
+    ]
+  },
 
   {
     path: '',
